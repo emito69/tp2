b5f31fbe247122daa57e0a1860bc52f9e4dcaa97
emito569 <emito569@hotmail.com>
2022-10-20 21:50:18 -0300

    versión 5

---
+++ js.js
@@ -0,0 +1,10 @@
+function totalAPagar() {
+	const total = document.getElementById('total');
+	const cant = document.getElementById('cantidad');
+	const categ = document.getElementById('categoria');
+
+	const calc = 200 * cantidad.value * categoria.value
+
+	total.value = "Total a Pagar: $ "+ calc;
+  }
+  
